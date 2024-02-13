e2e248c4b0dc0da7800191d4ce5eb4bb04a3f891
Jackson Sabey <jackson@juandefu.ca>
2024-02-13 00:32:16 -0800

    exit code on exit

--- mail.sh
+++ mail.sh
@@ -5,5 +5,7 @@ output=$(/opt/chronic/chronic $@)
 ec=$?
 
 if [ $ec -ne 0 ]; then
-	echo "$output" | /opt/sendmail/sendmail > /dev/null 2>&1
+	echo "$output" | /opt/sendmail/sendmail
 fi
+
+exit $ec
